b9fc4d4ee2605d23336b4cd5662557b09c0339bd
EL KACIMI Younes OBS/OINIS <cjft1912@orange.com>
2024-09-07 18:10:41 +0100

    Exercise 1.06: Project v0.4

--- part1/Project/index.js
+++ part1/Project/index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 
 app.get('/api',(req,res)=>{
-    res.send("Exercise 1.05: Project v0.3").status(200)
+    res.send("Message").status(200)
 })
 
 const PORT = process.env.PORT || 3000
