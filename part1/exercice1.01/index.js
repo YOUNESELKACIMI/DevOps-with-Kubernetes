1dbd4df73441279ae36accbe27218e9f818172ca
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2024-09-08 14:01:34 +0100

    Exercise 1.07: External access with Ingress

--- part1/exercice1.01/index.js
+++ part1/exercice1.01/index.js
@@ -1,3 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
 const randomString = Math.random().toString(16).substring(2,20)
 
 
@@ -6,7 +11,17 @@ const printString = () => {
     console.log(date+": "+randomString)
 }
 
-setInterval(printString,5000)
+app.get('/',(req,res)=>{
+    let date = new Date().toISOString()
+    let logString = date+": "+randomString
+    res.send(logString).status(200)
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
+})
 
 
 
