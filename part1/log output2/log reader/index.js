d353e30a607209df62c425ba77d0c7daed60502c
YOUNES EL KACIMI <youns.elkacimi@gmail.com>
2024-10-30 21:42:54 +0100

    Configured 2 replicas of Postgres using a statefullSet and a headless service

--- part1/log output2/log reader/index.js
+++ part1/log output2/log reader/index.js
@@ -3,8 +3,27 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+const {Sequelize,Model,DataTypes} = require('sequelize')
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+  });
 
+class Count extends Model {}
+Count.init({
+    count:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},{
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName:'count'
+})
+
+Count.sync({ force: true })
 
 let count = 0
 const url = "http://localhost:8081/log_Generator"
@@ -15,11 +34,13 @@ app.get('/log_reader',async (req,res)=>{
     console.log(response.data)
     const log = response.data.log
 
+    count++        
+    const countObject= {count}
+    console.log(countObject)
+    await Count.create(countObject)
 
     res.status(200).send(`<p>file content: this text is from file</p><p>env variable: ${process.env.MESSAGE}</p><p>${log}</p><p>${count}</p>`)
     //res.status(200).send(`${log} \n\n ${count}`)
-    count++
-
 })
 
 const PORT = process.env.PORT || 8082
